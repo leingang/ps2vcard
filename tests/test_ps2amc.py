bd6dd19271adc6dbcf04cfdf8a672243cd5bc41f
Matthew Leingang <leingang@nyu.edu>
2017-02-20 12:50:06 -0500

    Add TestCase methods to generate mock files.

--- tests/test_ps2amc.py
+++ tests/test_ps2amc.py
@@ -1,11 +1,58 @@
 #!/usr/bin/env python
 
+import csv
 import os.path
 import tempfile
 from subprocess import Popen,PIPE
 import unittest
+from jinja2 import Template
+
 
 class TestPs2Amc(unittest.TestCase):
+
+    def mock_psxls(self):
+        """ Mock up the ps.xls file.
+
+        This method is not a real test and doesn't need to be run often.
+        It won't be discovered by running this module on the command line,
+        or with `nose`.  To run it, execute
+
+            $ python -m unittest test_ps2amc.TestPs2Amc.mock_psxls
+
+        on the command line.
+        """
+        students=[]
+        input_path = os.path.join('data','mock.csv')
+        template_path = os.path.join('data','psxlst.html')
+        output_path = os.path.join('data','ps.xls')
+        with open(input_path) as data_fh, open(template_path) as template_fh:
+            template = Template(template_fh.read())
+            students = [student for student in csv.DictReader(data_fh)]
+        with open(output_path,'w') as output_fh:
+            output_fh.write(template.render(students=students))
+        self.assertTrue(os.path.exists(output_path))
+
+    def mock_pscsv(self):
+        """ Mock up the ps.csv file.
+
+        This method is not a real test and doesn't need to be run often.
+        It won't be discovered by running this module on the command line,
+        or with `nose`.  To run it, execute
+
+            $ python -m unittest test_ps2amc.TestPs2Amc.mock_pscsv
+
+        on the command line.
+        """
+        # csvfix order -f 2,3,5,9,10,11,12,13,14,15,16,17,18 mock.csv > ps.csv
+        input_path = os.path.join('data','mock.csv')
+        output_path = os.path.join('data','ps.csv')
+        fields = [2,3,5,9,10,11,12,13,14,15,16,17,18]
+        with open(input_path) as infile, open(output_path,'w') as outfile:
+            writer = csv.writer(outfile)
+            for line in csv.reader(infile):
+                writer.writerow([line[field-1] for field in fields])
+        self.assertTrue(os.path.exists(output_path))
+
 
     def setUp(self):
         self.infile = os.path.join('data','ps.csv')
