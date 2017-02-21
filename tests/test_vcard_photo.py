5692c2fcacb7e03c7847564b5f187b4448374aba
Matthew Leingang <leingang@nyu.edu>
2017-02-21 09:17:04 -0500

    refactor to work in any directory

--- tests/test_vcard_photo.py
+++ tests/test_vcard_photo.py
@@ -11,16 +11,19 @@ import vobject
 class TestVcardPhotoSerialize(unittest.TestCase):
 
     def setUp(self):
+        self._dir = os.path.dirname(__file__)
+        self.data_path = os.path.join(self._dir,'data')
+        self.golden_path = os.path.join(self._dir,'golden')
         self.card = vobject.vCard()
         self.card.add('n')
         self.card.n.value = vobject.vcard.Name(family="Thecat", given="Felix")
         self.card.add('fn')
         self.card.fn.value = "Felix Thecat"
         self.photo_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/229px-Felix_the_cat.svg.png"  # noqa E501
-        self.photo_path = os.path.join('data', 'felix-229.png')
+        self.photo_path = os.path.join(self.data_path, 'felix-229.png')
         if not os.path.isfile(self.photo_path):
             urllib.request.urlretrieve(self.photo_url, self.photo_path)
-        self.golden_filepath = os.path.join('golden', 'Felix.vcf')
+        self.golden_filepath = os.path.join(self.golden_path, 'Felix.vcf')
 
     def test_serialize_plain(self):
         gold = '\r\n'.join(['BEGIN:VCARD', 'VERSION:3.0', 'FN:Felix Thecat',
