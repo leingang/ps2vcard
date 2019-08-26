a61a169b4a96a615ed9274e83ed1abac18a0b969
Matthew Leingang <leingang@nyu.edu>
2019-08-26 09:59:38 -0400

    Add BeautifulSoup to install requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='ps2vcard',
     version='0.1',
     py_modules=['ps2vcard'],
-    install_requires=['Click', 'vobject', 'transitions'],
+    install_requires=['Click', 'vobject', 'transitions','bs4'],
     entry_points="""
         [console_scripts]
         ps2vcard=ps2vcard:convert_all
