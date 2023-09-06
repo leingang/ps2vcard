718c716cf1349f1931c9cb2a26ab8cd7ffcc81e7
Matthew Leingang <leingang@nyu.edu>
2023-09-06 09:03:40 -0400

    Create package and cli module within
    Part of a refactoring effort to break down one 900-line file into
    modules

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     install_requires=['Click', 'vobject', 'transitions','bs4','lxml'],
     entry_points="""
         [console_scripts]
-        ps2vcard=ps2vcard:convert_all
-        ps2vcard-old=ps2vcard:convert_all_from_frameset
-        ps2anki=ps2vcard:convert_to_anki
-        ps2amc=ps2vcard:convert_to_amccsv
-        psxls2amc=ps2vcard:convert_xls_to_amccsv
+        ps2vcard=ps2vcard.cli:convert_all
+        ps2vcard-old=ps2vcard.cli:convert_all_from_frameset
+        ps2anki=ps2vcard.cli:convert_to_anki
+        ps2amc=ps2vcard.cli:convert_to_amccsv
+        psxls2amc=ps2vcard.cli:convert_xls_to_amccsv
     """
 )
